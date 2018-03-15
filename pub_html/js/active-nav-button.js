d01d285772a1ae9033370948ca1fa2dd11c4b1a5
Unknown <sarahtong94@hotmail.com>
2018-03-15 12:23:04 -0700

    Attempted to create actives states for nav button
    work in progress

---
+++ pub_html/js/active-nav-button.js
@@ -0,0 +1,22 @@
+"use strict";
+
+console.log("Script is running!");
+
+// Get the container element
+// var mainNav = document.querySelector("main-nav");
+
+// Get all buttons with class="btn" inside the container
+// var navButtonActiveState = document.querySelector("button-nav:before");
+
+// Loop through the buttons and add the active class to the current/clicked button
+// for (var i = 0; i < navButtonActiveState.length; i++) {
+//   navButtonActiveState[i].addEventListener("click", function() {
+//     var current = document.getElementsByClassName("active");
+//     current[0].className = current[0].className.replace(" active", "");
+//     this.className += " active";
+//   });
+// }
+
+$(function() {
+  $('nav a[href^="/' + location.pathname.split("/")[1] + '"]').addClass('active');
+});
