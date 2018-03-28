5013a43f80d8f15779af2a854ba510a3253335f5
Unknown <sarahtong94@hotmail.com>
2018-03-27 17:49:27 -0700

    Made arrows visible on gallery
    for visual cue. only visible on tablet dimensions or larger. for smaller devices, arrows take up too much space

--- pub_html/js/smooth-div-scroll-initialize.js
+++ pub_html/js/smooth-div-scroll-initialize.js
@@ -2,14 +2,15 @@ $(document).ready(function() {
   $('#makeMeScrollable').smoothDivScroll({
     hotSpotScrolling: true,
     hotSpotScrollingStep: 5,
-    visibleHotSpotBackgrounds: "hover",
+    // visibleHotSpotBackgrounds: "hover",
+    visibleHotSpotBackgrounds: "always",
     easingAfterMouseWheelScrolling: false,
     touchScrolling: true
   });
 
-  // if ($(window).width() < 768) {
-  //   $('#makeMeScrollable').smoothDivScroll({
-  //     visibleHotSpotBackgrounds: "always"
-  //   });
-  // }
+  if ($(window).width() < 768) {
+    $('#makeMeScrollable').smoothDivScroll({
+      visibleHotSpotBackgrounds: "always"
+    });
+  }
 });
