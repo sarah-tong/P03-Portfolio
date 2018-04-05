124d0f3ad1f50a8b716ad2a20eda3c7e7f0ed2cb
Unknown <sarahtong94@hotmail.com>
2018-04-05 13:13:05 -0700

    removed unnecessary code
    in hamburger.js

--- pub_html/js/hamburger.js
+++ pub_html/js/hamburger.js
@@ -1,48 +1,25 @@
 "use strict";
 
-// console.log("Script is running!");
-
-var windowWidth = window.innerWidth;
-console.log("Window width: " + windowWidth);
-
-var navToggle = document.querySelector(".mobile-nav");
+var navToggle = document.querySelector(".hamburger-menu");
 var navWrapper = document.querySelector(".nav-wrapper");
-// var pageWrapper = document.querySelector(".page-wrapper");
-
-
-// if (windowWidth > 768) {
-//   navToggle.classList.add("open");
-//   navWrapper.classList.add("open");
-// }
-//
-// if (windowWidth < 768) {
-//   navToggle.classList.add("close");
-//   navWrapper.classList.add("close");
-// }
+var pageWrapper = document.querySelector(".main-content-wrapper");
 
 navToggle.addEventListener("click", function() {
   navToggle.classList.toggle("toggle");
   navWrapper.classList.toggle("toggle");
-  // pageWrapper.classList.toggle("toggle");
-  // if (navToggle.classList.contains("open")) {
-  //   console.log("#mobile-nav open, now closed.");
-  //
-  //   navToggle.classList.remove("open");
-  //   navWrapper.classList.remove("open");
-  //
-  //   navToggle.classList.add("close");
-  //   navWrapper.classList.add("close");
-  //
-  // } else {
-  //   console.log("#mobile-nav close, now open.");
-  //
-  //   navToggle.classList.add("open");
-  //   navWrapper.classList.add("open");
-  //
-  //   navToggle.classList.remove("close");
-  //   navWrapper.classList.remove("close");
-  // }
+  pageWrapper.classList.toggle("toggle");
+});
+
+window.addEventListener("resize", function() {
+  if (window.innerWidth > 768) {
+    if (navToggle.classList.contains("toggle")) {
+      navToggle.classList.remove("toggle");
+      navWrapper.classList.remove("toggle");
+      pageWrapper.classList.remove("toggle");
+    }
+  }
 });
 
 // nav: https://bootstrapious.com/p/bootstrap-sidebar
 // toggle: https://stackoverflow.com/questions/18880890/how-do-i-toggle-an-elements-class-in-pure-javascript
+// resize: https://www.w3schools.com/jsref/event_onresize.asp
