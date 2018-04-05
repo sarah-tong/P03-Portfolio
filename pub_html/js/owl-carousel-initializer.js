2bc708f8a04f41ff80c294406b1e56d824601ac9
Unknown <sarahtong94@hotmail.com>
2018-04-04 18:19:59 -0700

    Tested owl carousel plugin
    Did not work out. Plugin is buggy.

---
+++ pub_html/js/owl-carousel-initializer.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $('#owlAutoWidth').owlCarousel({
+			nav: true,
+			navText: ['', ''],
+			dots: false,
+			loop: false,
+			autoWidth: true
+		});
+
+    // used this owl.carousel.min.js file: https://gyx8899.github.io/YX-JS-ToolKit/owlCarousel2/owl-autoWidth.html
+});
