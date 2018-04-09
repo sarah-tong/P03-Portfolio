b1d7598fd176e13f2272337c3dff3a1f937c813b
Unknown <sarahtong94@hotmail.com>
2018-04-08 17:16:07 -0700

    MInified files
    minified js and css files

--- pub_html/js/swiper-initialize.js
+++ pub_html/js/swiper-initialize.js
@@ -1,14 +1,1 @@
-$(document).ready(function() {
-  //initialize swiper when document ready
-  var mySwiper = new Swiper('.swiper-container', {
-    // Optional parameters
-    loop: false,
-    autoHeight: true,
-    lazy: true,
-    slidesPerView: 'auto', // set width to auto: https://github.com/nolimits4web/Swiper/issues/742
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    }
-  })
-});
+$(document).ready(function(){var a=new Swiper(".swiper-container",{loop:false,autoHeight:true,lazy:true,slidesPerView:"auto",navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev",}})});
